316fb678de2e17e8c686bc5e95ac6267fe8ddc58
root <root@localhost.localdomain>
2018-10-27 17:58:03 +0100

    Updated algorithm

--- main.py
+++ main.py
@@ -1,87 +1,101 @@
-# WordSearch class
 class WordSearch(object):
     def __init__(self, grid):
         self.grid = grid
         self.ROW_LENGTH = len(grid)
-        self.DIRECTION_X = [1, 0]
-        self.DIRECTION_Y = [0, 1]
 
-    def searchWordInGrid(self, word, i, j):
-        # Recursive heap approach 
+    # The 'direction' variable represents either top to bottom (0) or left to right (1)
+    def searchWordInGrid(self, word, i, j, direction, actual):
+        # Recursive heap approach
         # Return true if word is empty
         if len(word) == 0:
             return True
 
-        # Check if first character is the same as the word's first character
-        if grid[i][0][j] is not word[0]:
+        # If out of bounds
+        if i >= self.ROW_LENGTH or j >= self.ROW_LENGTH:
             return False
-        i += 1
+
+        # Check if first character is the same as the word's first character
+        if grid[i][j] is not word[0]:
+            return False
+        if direction == 0:
+            i += 1
+        else:
+            j += 1
         word = word[1:]
+
+        # Return true if word is empty
+        if len(word) == 0:
+            return True
 
         # Optimisation to reduce the number of full word searches by also checking the last character of the string
         # Check if indexes won't get out of bounds
-        i_oob = i+len(word)-1 >= self.ROW_LENGTH
-        j_oob = j+len(word)-1 >= self.ROW_LENGTH
+        if direction == 0:
+            oob = i + len(word) - 1 >= self.ROW_LENGTH
+        else:
+            oob = j + len(word) - 1 >= self.ROW_LENGTH
+
+        if oob is True:
+            return False
 
         # Now check if the last character of the word appears either at the bottom, or right relative to the start of the word in the grid
-        if i_oob is False:
-            i_end = grid[i+len(word)-1][0][j] is not word[:-1]
+        i_end = True
+        j_end = True
+        if direction == 0:
+            i_end = grid[i+len(word)-1][j] is word[len(word)-1]
         else:
-            i_end = False
+            j_end = grid[i][j+len(word)-1] is word[len(word)-1]
 
-        if j_oob is False:
-            j_end = grid[i][0][j+len(word)-1] is not word[:-1]
-        else:
-            j_end = False
-
-        if i_end is False and j_end is False:
+        if i_end is False or j_end is False:
             return False
-        j -= 1
         word = word[:-1]
-
-        # If i overcomes or is equal to j, it means this is done
-        if i >= j:
-            return True
 
         # Now calculate the middle of the word
         mid = len(word)/2
         if len(word)%2 == 1:
-            addCoord = int(mid+0.5)
+            addCoord = int(mid - 0.5)
         else:
             addCoord = int(mid)
 
         # Finally return the same method call for the first half and second half from both top to bottom and left to right
-        return (self.searchWordInGrid(word[:int(mid)], i, j)
-                and (
-                        self.searchWordInGrid(word[int(mid):], i + addCoord, j)
-                        or self.searchWordInGrid(word[int(mid):], i, j + addCoord)
+        if direction == 0:
+            return (self.searchWordInGrid(word[:int(mid)], i, j, direction, actual)
+                    and self.searchWordInGrid(word[int(mid):], i + addCoord, j, direction, actual)
                 )
-            )
+        else:
+            return (self.searchWordInGrid(word[:int(mid)], i, j, direction, actual)
+                    and self.searchWordInGrid(word[int(mid):], i, j + addCoord, direction, actual)
+                )
 
 
     def is_present(self, word):
         # For each coordinates i and j in the grid, check if the word exists
         for i in range(self.ROW_LENGTH):
             for j in range(self.ROW_LENGTH):
-                if self.searchWordInGrid(word, i, j) is True:
+                if self.searchWordInGrid(word, i, j, 0, word) is True or self.searchWordInGrid(word, i, j, 1, word) is True:
                     return True
         return False
 
 
+# Create method to generate random grid
+import string, random
+
+def random_grid(LEN):
+    a = [[0 for x in range(LEN)] for y in range(LEN)]
+    for i in range(LEN):
+        for j in range(LEN):
+            a[i][j] = random.choice(string.ascii_uppercase)
+    return a
+
+
 # Defining grid to check and words to find
 
-words_to_find = ['DINOSAUR', 'YES', 'PEXIP', 'IS', 'SO', 'COOL']
-grid = [
-    ['PEXIPA'],
-    ['ADSSOL'],
-    ['CDOLSD'],
-    ['OSADSA'],
-    ['OKJBFK'],
-    ['LSDASD']
-]
+words_to_find = ['AS', 'YES', 'PEXIP', 'IS', 'SO', 'COOL']
+grid = random_grid(1000)
 
 
 # Testing the code
+
+# lines = [line.rstrip('\n').replace("\'", '').upper() for line in open('/usr/share/dict/words')]
 
 ws = WordSearch(grid)
 
